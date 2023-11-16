b89c3681c0876c25fbe3a7094c30aa5e43f434a5
gdmrw <gdmr_w@outlook.com>
2023-11-16 11:00:25 -0800

    git ignore test

---
+++ ex9_copy/client/src/Sidebar.js
@@ -0,0 +1,16 @@
+import "./Sidebar.css";
+function SidebarContent() {
+    return (
+        <>
+            <div className="sidenav">
+                <a href="#about">About</a>
+                <a href="#services">Friends</a>
+                <a href="#clients">Animals</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </>
+    );
+}
+export default SidebarContent;
+
+
