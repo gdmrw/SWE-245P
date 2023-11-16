503e8a909a09d938193cbd72f2785ddae7f0baf9
gdmrw <gdmr_w@outlook.com>
2023-11-15 21:09:27 -0800

    first react try succeed

---
+++ ex7/client/src/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+function Footer() {
+    return (
+    <>
+        <div className="footer">
+            <p>Footer</p>
+        </div>
+    </>
+    );
+}
+
+export default Footer;
